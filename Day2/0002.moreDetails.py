3951331e0b15c3668dc1029ada3807bc335c4b5e
Arvinderpal10 <arvinderpal10@gmail.com>
2024-12-15 19:48:01 +0530

    Day 2 Python and tools setup

---
+++ Day2/0002.moreDetails.py
@@ -0,0 +1,21 @@
+print("THis is my first line of code")
+print("THis is my Second line of code")
+
+
+# Print a Z pattern using print statements 
+
+print("* * * * *")
+print("      *")
+print("    *")
+print("  *")
+print("* * * * *")
+
+
+# Print - It is a function that prints the given string to the console 
+# It is inbuilt function 
+
+# Hola --  Hello in Spanish 
+print("Hola")
+print("")
+
+# print("Hello")
