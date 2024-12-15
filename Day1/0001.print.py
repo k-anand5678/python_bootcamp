0072e7912d103dab958c2cb5da2b15922671a7a8
Arvinderpal10 <arvinderpal10@gmail.com>
2024-12-15 19:46:51 +0530

    Day 1 python setup

---
+++ Day1/0001.print.py
@@ -0,0 +1,13 @@
+# Hello World 
+
+print("Hello World")
+
+#Why hello world? 
+## Because it is the easiest program to write and understand 
+# Basic python syntax
+## TO make sure the setup is working fine -- Python is installed and VS Code is setup properly 
+
+
+
+# Console -- Output Window 
+
