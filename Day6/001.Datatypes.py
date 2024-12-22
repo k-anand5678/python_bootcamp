cac4c9c1794da9bc8edcfb6b47909fe47da24409
Arvinderpal10 <arvinderpal10@gmail.com>
2024-12-22 17:07:28 +0530

    Datatypes in Python

---
+++ Day6/001.Datatypes.py
@@ -0,0 +1,45 @@
+# Data Type : -- Types of values that we can store ina variable
+
+# Marksheet : 
+# Class - 9 Name : Kyass Marks - 90, 86, 87, 78,89  --Avg : 86.5
+
+# x = "Hello"  - String type 
+# x = 30 ----   Nyumeric type -- Integer
+# x = 40.6     --- Numeric type --- Float
+# x = True / False  --- boolean -- 
+ 
+x = "Hello"
+# x = 30 
+# x = 40.6
+# x = False    
+# True == 1 / False == 0  
+
+# binary Langauge -- 0 ,1 
+# print(x+10)
+
+# type() -- Will return the type of the variable 
+
+# print(type(x))
+
+
+y = 200
+z = "Something"
+t = True
+a = 100.45
+
+
+print(type(y))
+print(type(z))
+print(type(t))
+print(type(a))
+
+print("Hello")   # HELLO - H - 72 --> In the sequence of 0 and 1
+print(30) #  30 -- In the sequence of 0 and 1 
+
+# ASCII Code -- H  
+# c = 'p'
+# print("The ASCII value of '" + c + "' is", ord(c))
+
+
+
+
