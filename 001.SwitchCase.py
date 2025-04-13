6cab7795233791d3b4f7e3189986e379ff228bd5
Arvinder Pal <54645908+Arvinderpal10@users.noreply.github.com>
2025-04-13 17:06:23 +0530

    Finding the position of the point in cartesian system

---
+++ 001.SwitchCase.py
@@ -0,0 +1,7 @@
+"""
+
+Write a Python code to find whether the given point lies on x-axis , y-axis or in x-y plane or is origin.
+
+Input Format: (x,y)
+Output: The number lies in x-axis/y-axis/is origin / x-y plane.
+"""
